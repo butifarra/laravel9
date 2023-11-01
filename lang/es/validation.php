cdc5a6d08bdcfd5b73b95b1cd85b9c30108f4d01
Denis Pujadas <elbutifarra@gmail.com>
2023-11-01 19:29:43 -0300

    Hello
    Very good

---
+++ lang/es/validation.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'required' => 'El campo :attribute es obligatorio',
+    'attributes' => [
+        'title' => 'títutlo',
+        'body' => 'contenido'
+    ],
+    'min' => [
+        'array' => 'The :attribute field must have at least :min items.',
+        'file' => 'The :attribute field must be at least :min kilobytes.',
+        'numeric' => 'The :attribute field must be at least :min.',
+        'string' => 'El :attribute debe tener al menos 4 caracteres.'
+    ]
+
+];
